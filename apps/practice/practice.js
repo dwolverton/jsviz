12f0b651ef8212e0605499c54cf6fb9e2cbe11fe
David Wolverton <dwolvert@gmail.com>
2017-02-12 17:23:33 -0500

    added progress and set selection UI to practice

--- apps/practice/practice.js
+++ apps/practice/practice.js
@@ -1,10 +1,10 @@
 (function(){
 var app = angular.module("jsVizPractice", ["jsViz"]);
-app.controller("main", function($scope, $interval, $rootScope, jsvizSync, problemSetService, configService) {
+app.controller("main", function($scope, $interval, jsvizSync, problemSetService) {
     $scope.variables = {};
     $scope.isSolved = isSolved;
     $scope.showInstructions = orDefault(localStorage.jvizShowInstructions, "true") === "true";
-    $scope.next = window.next = nextProblem;
+    $scope.next = window.next = problemSetService.getNextProblem;
     $scope.reset = window.reset = reset;
     $scope.toggleInstructions = function() {
         $scope.showInstructions = !$scope.showInstructions;
@@ -17,20 +17,12 @@ app.controller("main", function($scope, $interval, $rootScope, jsvizSync, proble
         });
     }
 
+    $scope.problemSetService = problemSetService;
+
     var syncer = jsvizSync.buildSyncFunction(window, $scope.variables);
     $interval(syncer.sync, 300);
 
-    $rootScope.$on("$locationChangeSuccess", function() {
-        configService.loadFromUrl();
-        problemSetService
-            .loadDataSets(configService.get('sourceUrl'), configService.get('setIds'))
-            .then(nextProblem);
-    });
-
-    function nextProblem() {
-        problemSetService.getNextProblem();
-        reset();
-    }
+    $scope.$watch("problemSetService.getCurrentProblem()", reset, true);
 
     function reset() {
         var problem = problemSetService.getCurrentProblem();
@@ -58,36 +50,81 @@ app.controller("main", function($scope, $interval, $rootScope, jsvizSync, proble
     }
 });
 
-app.factory("problemSetService", function($http) {
-    var fullSet = [];
-    var currentProblemIndex = Infinity;
-    var availableSets = {};
-    var randomOrder = false;
+app.controller("infoBar", function($scope, problemSetService, setsSource, configService) {
+    window.cs = configService;
 
-    function loadDataSets(sourceUrl, setIds) {
-        return $http.get(sourceUrl).then(function(response) {
-            var includeAllSets = !setIds || _.isEmpty(setIds);
-            var sets = response.data.problemSets;
-            randomOrder = orDefault(response.data.randomOrder, false);
+    $scope.problemSetService = problemSetService;
+    $scope.config = configService.config;
+    $scope.setsSource = setsSource;
+    $scope.sets = [];
 
-            var practiceProblems = [];
-            availableSets = {};
-            _.forOwn(sets, function(set, setId) {
-                var included = includeAllSets || _.includes(setIds, setId);
-                availableSets[setId] = included;
-                if (included) {
-                    practiceProblems = practiceProblems.concat(set);
-                }
-            });
-            return practiceProblems;
-        }).then(function(practiceProblems) {
-            fullSet = practiceProblems;
-            currentProblemIndex = Infinity;
+    $scope.$watch("config.setIds", rebuildSets, true);
+    $scope.$watch("setsSource.getAllSetIds()", rebuildSets, true);
+
+    function rebuildSets() {
+        $scope.sets = [];
+        var activeIds = _.includes(configService.get('setIds'));
+        _.forEach(setsSource.getAllSetIds(), function(setId) {
+            var active = _.includes(configService.get('setIds'), setId);
+            $scope.sets.push({
+                id: setId,
+                active: active
+            })
         });
+    }
+
+    $scope.toggleSet = function(setId, active) {
+        var activeIds = configService.get('setIds');
+        if (active && !_.includes(activeIds, setId)) {
+            activeIds.push(setId);
+        }
+        if (!active) {
+            activeIds = _.without(activeIds, setId);
+        }
+        configService.set('setIds', activeIds);
+    }
+});
+
+app.factory("problemSetService", function($rootScope, setsSource, configService) {
+    var EMPTY_SET = [{goal: {}}];
+    var fullSet = EMPTY_SET;
+    var currentProblemIndex = 0;
+    var randomOrder;
+
+    function buildSet() {
+        var setIds = configService.get('setIds');
+        var sets = setsSource.getAllSets();
+        randomOrder = setsSource.isRandomOrder();
+
+        var includeAllSets = !setIds || _.isEmpty(setIds);
+
+        var practiceProblems = [];
+        _.forOwn(sets, function(set, setId) {
+            var included = includeAllSets || _.includes(setIds, setId);
+            if (included) {
+                practiceProblems = practiceProblems.concat(set);
+            }
+        });
+        if (randomOrder) {
+            practiceProblems = _.shuffle(practiceProblems);
+        }
+        if (_.isEmpty(practiceProblems)) {
+            practiceProblems = EMPTY_SET;
+        }
+        fullSet = practiceProblems;
+        currentProblemIndex = 0;
     }
 
     function getCurrentProblem() {
         return fullSet[currentProblemIndex];
+    }
+
+    function getCurrentProblemNumber() {
+        return currentProblemIndex + 1;
+    }
+
+    function getProblemCount() {
+        return fullSet.length;
     }
 
     function getNextProblem() {
@@ -101,14 +138,62 @@ app.factory("problemSetService", function($http) {
         return getCurrentProblem();
     }
 
+    var scope = $rootScope.$new(true);
+    scope.config = configService.config;
+    scope.$watch("config.setIds", buildSet);
+    scope.setsSource = setsSource;
+    scope.$watch("setsSource.getAllSets()", buildSet, true);
+
     return {
-        loadDataSets: loadDataSets,
         getCurrentProblem: getCurrentProblem,
-        getNextProblem: getNextProblem
+        getCurrentProblemNumber: getCurrentProblemNumber,
+        getNextProblem: getNextProblem,
+        getProblemCount: getProblemCount
     }
 });
 
-app.factory("configService", function($location) {
+app.factory("setsSource", function($rootScope, $http, configService) {
+    var sourceUrl;
+    var allSets = {};
+    var randomOrder = false;
+
+    function getAllSets() {
+        return allSets;
+    }
+
+    function getAllSetIds() {
+        return Object.keys(allSets);
+    }
+
+    function isRandomOrder() {
+        return randomOrder;
+    }
+
+    function setSourceUrl(newSourceUrl) {
+        console.log("a", newSourceUrl);
+        if (newSourceUrl !== sourceUrl) {
+            sourceUrl = newSourceUrl;
+            if (sourceUrl) {
+                $http.get(sourceUrl).then(function(response) {
+                    allSets = response.data.problemSets;
+                    randomOrder = orDefault(response.data.randomOrder, false);
+                });
+            }
+        }
+    }
+
+    var scope = $rootScope.$new(true);
+    scope.config = configService.config;
+    scope.$watch("config.sourceUrl", setSourceUrl);
+
+    return {
+        getAllSets: getAllSets,
+        getAllSetIds: getAllSetIds,
+        isRandomOrder: isRandomOrder
+    }
+});
+
+app.factory("configService", function($rootScope, $location) {
 
     var config = {};
 
@@ -120,6 +205,15 @@ app.factory("configService", function($location) {
     function set(option, value, defaultValue) {
         value = orDefault(value, defaultValue);
         config[option] = value;
+
+        switch (option) {
+        case "setIds":
+            $location.search("sets", value.join(","));
+            break;
+        case "sourceUrl":
+            $location.search("sourceUrl", value);
+            break;
+        }
     }
 
     function parseArray(string) {
@@ -129,14 +223,20 @@ app.factory("configService", function($location) {
         return string.toString().split(/[\/,]/);
     }
 
+    $rootScope.$on("$locationChangeSuccess", function() {
+        loadFromUrl();
+    });
+
     return {
-        loadFromUrl: loadFromUrl,
         get: function(option) {
             return config[option];
         },
-        set: set
+        set: set,
+        config: config
     }
 });
+
+
 
 function orDefault(value, defaultValue) {
     if (typeof value === "undefined") {
